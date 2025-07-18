a2c183f0d7af34d48878f33cb2ac7594e4c5300b
raghavshulka <himanshu4shukla4l@gmail.com>
2025-07-18 21:13:36 +0530

    created frontend initial verson

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  headers: async () => {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp'
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin'
+          }
+        ]
+      }
+    ]
+  }
   /* config options here */
 };
 
